046498b3e765a916dea7396837ecf4f12f744fa5
king <2962938812@qq.com>
2019-04-02 20:31:45 +0800

    v0.1

--- src/main/java/enjoy/git/Example.java
+++ src/main/java/enjoy/git/Example.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class Example {
     @RequestMapping("/")
     String home() {
-        return "Hello Git!";
+        return "Hello Git! -V.01";
     }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
