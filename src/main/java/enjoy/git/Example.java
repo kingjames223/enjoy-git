aeb4936bbcb93dbeed326293134cd7e2e3dd7bac
king <2962938812@qq.com>
2019-04-02 20:30:50 +0800

    new git

---
+++ src/main/java/enjoy/git/Example.java
@@ -0,0 +1,16 @@
+package enjoy.git;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "Hello Git!";
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
+
