222ec52bf11c98f70647a07a7ca5bc4d9e3905f3
king <2962938812@qq.com>
2019-04-03 16:05:17 +0800

    war-jenkins

---
+++ src/main/java/enjoy/git/App.java
@@ -0,0 +1,19 @@
+package enjoy.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer  {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
+
+}
+
---
+++ src/main/java/enjoy/git/UserController.java
@@ -0,0 +1,16 @@
+package enjoy.git;
+
+/**
+ * @author 【享学课堂】 King老师
+ */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @RequestMapping("/hello")
+    public Object sayHello() {
+        return "hello";
+    }
+}
+
