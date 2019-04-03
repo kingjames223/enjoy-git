6904ebbf9deb695568bee60f89b93751a3226bc7
king <2962938812@qq.com>
2019-04-03 17:20:17 +0800

    war-jenkins-v.01

--- src/main/java/enjoy/git/UserController.java
+++ src/main/java/enjoy/git/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    @RequestMapping("/hello")
+    @RequestMapping("/hello---111")
     public Object sayHello() {
         return "hello";
     }
